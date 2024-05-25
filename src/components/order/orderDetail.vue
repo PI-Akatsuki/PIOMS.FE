5bacc439b582c0d87b183187cbc0e23503171d8c
신동호 <101622086+letsplaycoding@users.noreply.github.com>
2024-05-25 20:53:19 +0900

    Revert "change port, order page finished"

--- src/components/order/orderDetail.vue
+++ src/components/order/orderDetail.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="popup-overlay" >
        <div class="popup-content">
-           <button class="close" @click="showDetailPopup" >돌아가기</button>
+           <span class="close" @click="showDetailPopup">&times;</span>
+           
            <br>
-          
-           <br>
-                <h1 align="center">발주서</h1>
-
            <div class="info">
+                <h2 align="center">발주서</h2>
                 <div style="display:flex;" align="center">
                   <div class="divvv-title">
                     수신처      
@@ -22,7 +20,7 @@
                 
                     <div class="divvv">
                         <div class="divvv2" >
-                          <div class="divvv3-title">
+                          <div class="divvv3">
                             관리자
                           </div>
                           <div class="divvv3">
@@ -30,7 +28,7 @@
                           </div>
                         </div>
                         <div class="divvv2">
-                          <div class="divvv3-title">
+                          <div class="divvv3">
                               관리자명 
                           </div>
                           <div class="divvv3">
@@ -38,18 +36,18 @@
                           </div>
                         </div>
                         <div class="divvv2">
-                          <div class="divvv3-title">
-                            점주전화번호
+                          <div class="divvv3">
+                            sadfsadf 
                           </div>
                           <div class="divvv3">
-                             {{item.franchiseOwnerPhone}}
+                             asdfsadf
                           </div>
                         </div>
                     </div>
 
                     <div class="divvv">
                       <div class="divvv2" >
-                          <div class="divvv3-title">
+                          <div class="divvv3">
                             가맹점
                           </div>
                           <div class="divvv3">
@@ -57,7 +55,7 @@
                           </div>
                         </div>
                         <div class="divvv2">
-                          <div class="divvv3-title">
+                          <div class="divvv3">
                               점주명 
                           </div>
                           <div class="divvv3">
@@ -65,11 +63,12 @@
                           </div>
                         </div>
                         <div class="divvv2">
-                          <div class="divvv3-title">
-                            점주전화번호
+                          <div class="divvv3">
+                            가맹점주소 
                           </div>
                           <div class="divvv3">
-                             {{item.franchiseOwnerPhone}}
+                            asdf
+                             <!-- {{ item.franchiseAddress }} -->
                           </div>
                         </div>
                 </div>  
@@ -96,7 +95,7 @@
               </table>
             </div>
             <br><br>
-            <div class="divvv-title" style="border-top: 2px black solid;">
+            <div class="divvv-title">
                 반품상품
             </div>
             <div style="display: block; font-size: 1.5lh; display:flex; justify-content: center; font-weight: 1000; width:100%" align = "center">
@@ -121,45 +120,12 @@
             <br>
             <br>
             <div class="but-group">
-              <input class="but" type="button" value="발주승인" @click="accpetOrder">
-              <input class="but" type="button" value="발주반려" @click="clickDeny">
-            </div>
-
-            <div v-if="isDenied">
-              <div class="popup-overlay"  >
-                <div class="popup-content" style="width: 600px; height: 600px;">
-                  <form @submit.prevent="denyOrder">
-                    <h2>반려사유 </h2>
-                    <textarea v-model="reason" cols="30" rows="5"></textarea><br>
-
-                    <div style="display: block; font-size: 1.5lh; display:flex; justify-content: center; font-weight: 1000; width:100%;" align="center">
-                      <table>
-                        <thead >
-                          <tr>
-                            <th  v-for="(header, index) in headers" :key="index" >{{ header.label }}</th>
-                          </tr>
-                        </thead>
-                        <tbody >
-                          <tr class="table" v-for="(product, rowIndex) in list" :key="rowIndex">
-                            <td v-for="(header, colIndex) in headers" :key="colIndex">
-                              <div>{{ product[header.key] }}</div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                    <div class="but-group">
-                      <button type="button" class="close" @click="clickDeny">취소</button>
-                      &nbsp;&nbsp;&nbsp;
-                      <button type="submit" class="close">등록</button>
-                    </div>
-                  </form> 
-                </div>
-              </div>
+              <input class="but" type="button" value="발주승인">
+              <input class="but" type="button" value="발주반려">
             </div>
             
           </div>
-          발주일자 : {{ item.orderDate }}
+          배송 예정일 : {{ item.deliveryDate }}
            <br>
            주문코드 : {{ item.orderCode }}<br>   
        </div>
@@ -172,8 +138,6 @@
 
 <script setup>
    import { ref } from "vue";
-   import axios from 'axios';
-
    const headers = ref([
     /*{ key: 'requestProductCode', label: '상품 코드' },*/
     { key: 'productName', label: '상품명' },
@@ -193,73 +157,194 @@
  const item = props.detailItem;
  const list = props.detailItem.orderProductList;
  const exchangeList = props.detailItem.exchangeProductList;
-
- // 추후 개선 예정 
- const adminCode = 2;
- const franchiseCode = 1;
-
  console.log(item);
  console.log(list);
  console.log(exchangeList);
-
-
-const accpetOrder = async () => {
-    try {
-      const response = await fetch(`/api/admin/order/${item.orderCode}/accept?adminCode=${adminCode}`, {
-        method: 'PUT',
-      });
-      if(response.status ==406){
-        alert("이 발주는 이미 처리되어 있습니다.");
-        props.showDetailPopup();
-        return;
-      }
-      if (!response.ok) {
-        alert("헉 왜 주문 승인 안되지????????????")
-        throw new Error('네트워크 오류 발생');
-      }
-      props.showDetailPopup();
-
-    } catch (error) {
-      console.error('오류 발생:', error);
-    }
-};
-
-const isDenied = ref(false);
-const reason = ref('');
-const clickDeny = () =>{
-  isDenied.value = !isDenied.value;
-}
-
-const denyOrder = async () => {
-  console.log(item.orderCode);
-    try {
-      const response = await fetch(`/api/admin/order/${item.orderCode}/deny?adminCode=${adminCode}&denyMessage=${reason.value}`, {
-        method: 'PUT',
-      });
-      if(response.status ==406){
-        alert("이 발주는 이미 처리되어 있습니다.");
-        clickDeny();
-        props.showDetailPopup();
-        return;
-      }
-      if (!response.ok) {
-        alert("헉 왜 주문 거절 안되지????????????")
-        throw new Error('네트워크 오류 발생');
-      }
-    } catch (error) {
-      alert("헉 왜 주문 거절 안되지????????????");
-      console.error('오류 발생:', error);
-    }
-};
-
-
 
 </script>
 
 
 <style scoped>
 
+  .popup-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+  }
 
+  
+  
+  .popup-content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 30px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    width: 90%;
+    max-width: 2000px;
+    height: 80%;
+    overflow-y: auto;
+    max-height: 80vh;
+  }
+  
+  .popup-content h2 {
+    margin-bottom: 20px;
+  }
+  
+  .popup-content .form-group {
+    margin-bottom: 20px;
+  }
+  
+  .popup-content label {
+    font-weight: bold;
+  }
+  
+  .popup-content input[type="text"],
+  .popup-content textarea {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    resize: none;
+    height: auto;
+  }
+  
+  .popup-content button {
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  .popup-content button:hover {
+    background-color: #45a049;
+  }
+  .but-group {
+    display: flex;
+    justify-content: right;
+    align-items: center;
+  }
+  
+  .but {
+    margin-bottom: 10px;
+    margin-right: 10px;
+    border-radius: 10px;
+    border: 1px solid #D9D9D9;
+    background-color: #D9D9D9;
+    color: #000000;
+    font-size: 20px;
+    font-weight: 400;
+    font-family: 'Inter', sans-serif;
+    width: 10%;
+  }
+  
+  .but:hover {
+    background-color: #333333;
+  }
+  
+  .but.active {
+    background-color: #FEDB56;
+    border-color: #FEDB56;
+  }
+  
+  .popup-content::-webkit-scrollbar {
+    display: none;
+  }
+  
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    border: 1.5px solid #000000;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+    text-align: left;
+    
+  }
+  
+  
+  .pagination {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  button {
+    margin: 0 5px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+  
+  .highlighted {
+    background-color: hotpink;
+  }
 
+  .divvv-title{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #D9D9D9; 
+    height:100%; 
+    width:99.9%; 
+    font-size: 1lh;
+    font-weight: 1000;
+    border: 1px solid #000000;    
+    padding-top: 5px;
+    padding-bottom: 5px;
+    /* padding: 5px; */
+  }
+
+  .divvv {
+    display: block;
+    justify-content: center;
+    align-items: center;
+    background-color: white; 
+    height:100%; 
+    width:50%; 
+    border: 1px solid #000000;    
+  }
+  
+  .divvv2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white; 
+    height: 33.33333333%;
+    width:100%; 
+    border-right: 1px solid #000000;    
+    border-bottom: 1px solid #000000;    
+  }
+  .divvv3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width:100%; 
+    font-size: 20px;
+    border-left: 1px solid #000000;    
+  }
+
+  .info{
+    border: 3px solid #000000;    
+    
+  }
   </style>
   
--- src/components/order/orderPopup.vue
+++ src/components/order/orderPopup.vue
@@ -1,56 +1,39 @@
 <template>
     <div class="popup-overlay" v-if="writeActive">
       <div class="popup-content">
+        <span class="close" @click="showPopup">&times;</span>
         
-        
-        <div class="info">
         <h2 align="center">발주서 생성</h2>
         <br /><br /><br />
-
-      <div class="filter-container">
-          <div class="radio-group">
-            <div class="title"><label style="width:100px">검색 </label></div>
-            <input v-model="filter" placeholder="검색어를 입력하세요" @input="applyFilter" />
-          </div>
-
-          <div class="radio-group">
-            <div class="title"><label style="width:100px">상품상태</label></div>
-
-            <label>
-              전체 <input type="radio" value="" name="dateOrder" v-model="conditionFilter" @change="applyFilter" checked>
-            </label>
-            <label>
-              공급가능 <input type="radio" value="공급가능" name="dateOrder" v-model="conditionFilter" @change="applyFilter" >
-            </label>
-            <label>
-              일시제한 <input type="radio" value="일시제한" name="dateOrder" v-model="conditionFilter" @change="applyFilter" >
-            </label>
-            <label>
-              단종 <input type="radio" value="단종" name="dateOrder" v-model="conditionFilter" @change="applyFilter" >
-            </label>
-            <label>
-              품절 <input type="radio" value="품절" name="dateOrder" v-model="conditionFilter" @change="applyFilter">
-            </label>
-          </div>
-      </div>
-
-    <div align="center">
-      <h3>상품 리스트</h3>
-      <div class="table-wrapper" align="center">
+        <div>
+          <input v-model="filter" placeholder="검색어를 입력하세요" @input="applyFilter" />
+          <select v-model="conditionFilter" @change="applyFilter">
+            <option value="">주문 상태</option>
+            <option value="공급가능">공급가능</option>
+            <option value="일시제한">일시제한</option>
+            <option value="단종">단종</option>
+            <option value="품절">품절</option>
+          </select>
           <table>
             <thead>
               <tr>
-                <th>상품 코드</th><th>상품 이름</th><th>상품 가격</th><th>본사 수량</th><th>상품 상태</th><th>색상</th><th>상품 설명</th><th>상품카테고리</th><th>성별</th>
+                <th>상품 코드</th>
+                <th>상품 이름</th>
+                <th>상품 가격</th>
+                <th>본사 수량</th>
+                <th>상품 상태</th>
+                <th>색상</th>
+                <th>상품 설명</th>
+                <th>상품카테고리</th>
+                <th>성별</th>
               </tr>
             </thead>
-
             <tbody>
               <tr v-for="(product, index) in filteredLists" :key="index" 
                   :id="'row-' + index"
                   @dblclick="addProductToList(product)" 
                   @mouseenter="highlightRow(index)"
-                  @mouseleave="resetRowColor(index)"
-                  style="cursor: pointer;">
+                  @mouseleave="resetRowColor(index)">
                 <td>{{ product.productCode }}</td>
                 <td>{{ product.productName }}</td>
                 <td>{{ product.productPrice }}원</td>
@@ -64,11 +47,9 @@
             </tbody>
           </table>
         </div>
-      </div>
-        <br>
-        <div align="center">
+        <br />
         <h3>선택된 상품 리스트</h3>
-        <div class="table-wrapper2">
+        <div></div>
         <table>
           <thead>
             <tr>
@@ -84,23 +65,18 @@
             <td>{{ selectedProduct.productName }}</td>
             <td>{{ selectedProduct.productCount }}</td>
             <td><input type="number" v-model="selectedProduct.quantity" min="1" @change="calculateTotalPrice" /></td>
-            <td><button class="button2" @click="removeProductFromList(index)">취소</button></td>
+            <td><button @click="removeProductFromList(index)">취소</button></td>
           </tr>
         </table>
-      </div >
-    </div>
-      <div style="display: flex; justify-content: right;">
         <p v-if="totalPrice > 0">총 가격: {{ totalPrice }}원</p>
-        <button @click="exportOrder">발주신청하기</button>
-        <button class="close" @click="showPopup" >돌아가기</button>
       </div>
-      </div>
+      <button @click="exportOrder">발주신청하기</button>
     </div>
-  </div>
+    
 
   </template>
   
-<script setup>
+  <script setup>
   import { ref } from "vue";
   
   const props = defineProps({
@@ -115,10 +91,6 @@
   const filteredLists = ref([]);
   
   const applyFilter = () => {
-    if(conditionFilter.value == ""){
-      filteredLists.value = products.value;
-      return;
-    }
     filteredLists.value = products.value.filter((item) => {
       const matchesFilter = filter.value
         ? item.productName.toLowerCase().includes(filter.value.toLowerCase()) ||
@@ -137,7 +109,6 @@
   
   const getProducts = async () => {
     try {
-
       const response = await fetch("/api/franchise/product", {
         method: "GET",
       });
@@ -148,7 +119,6 @@
       products.value = data.map(({ orderProductList, ...rest }) => ({ ...rest, quantity: 1 }));
       filteredLists.value = products.value;
       console.log(products);
-      
     } catch (error) {
       console.error("오류 발생:", error);
     }
@@ -190,20 +160,19 @@
     acc[product.productCode] = product.quantity;
     return acc;
   }, {});
-
   const orderData = {
-    // products: productsData,
+    products: productsData,
     franchiseCode: 1 // 실제 프랜차이즈 코드를 사용 예정
   };
-
   try {
     const response = await fetch("/api/franchise/1/order", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(orderData)
-      });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(orderData)
+    });
+    
     if (response.status == 406) {
       alert("생각은 하셨나요? 신청 재고량 너무 많거나, 이미 승인 대기중인 발주가 존재합니다.");
       props.showPopup();
@@ -227,6 +196,146 @@
   </script>
   
   <style scoped>
-
-</style>
+  /* 수정 팝업 스타일 */
+  .popup-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+  }
+  
+  .popup-content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 30px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    width: 90%;
+    max-width: 2000px;
+    height: 80%;
+    overflow-y: auto;
+    max-height: 80vh;
+  }
+  
+  .popup-content h2 {
+    margin-bottom: 20px;
+  }
+  
+  .popup-content .form-group {
+    margin-bottom: 20px;
+  }
+  
+  .popup-content label {
+    font-weight: bold;
+  }
+  
+  .popup-content input[type="text"],
+  .popup-content textarea {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    resize: none;
+    height: auto;
+  }
+  
+  .popup-content button {
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  .popup-content button:hover {
+    background-color: #45a049;
+  }
+  
+  .img {
+    height: 90%;
+    width: 90%;
+  }
+  
+  .imgForm {
+    background-color: #b7b7b7;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+    padding: 1px 15px 15px;
+    margin: 10px;
+  }
+  
+  .but-group {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .but {
+    margin: 20px;
+    padding: 10px;
+    border-radius: 10px;
+    border: 1px solid #D9D9D9;
+    background-color: #D9D9D9;
+    color: #000000;
+    font-size: 30px;
+    font-weight: 600;
+    font-family: 'Inter', sans-serif;
+    width: 30%;
+  }
+  
+  .but:hover {
+    background-color: #333333;
+  }
+  
+  .but.active {
+    background-color: #FEDB56;
+    border-color: #FEDB56;
+  }
+  
+  .popup-content::-webkit-scrollbar {
+    display: none;
+  }
+  
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+    text-align: left;
+  }
+  
+  .pagination {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  button {
+    margin: 0 5px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+  
+  .highlighted {
+    background-color: hotpink;
+  }
+  </style>
   
